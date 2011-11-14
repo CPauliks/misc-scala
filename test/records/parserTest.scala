2ba876cd41e9560bc70cfb752bb6ff97771748cc
Christopher Pauliks <massconfuzion@gmail.com>
2011-11-14 15:47:14 -0600

    Some more things we can use in the REPL part of this

--- test/records/parserTest.scala
+++ test/records/parserTest.scala
@@ -5,8 +5,14 @@ object ParserTest {
 	val s = "while(y){n = n.next.last,\ny = 0};\n".stripLineEnd.stripSuffix(";")
 	
     def main(args: Array[String]) {
-	    println(s)
+	    //println(s)
 	    val p = StatementParser.parseAll(StatementParser.statement, s)
-	    println(p)
+	    if (p.successful){
+	      println(p.get)
+	      //println(execute(store)(p.get))
+	    }
+	    else {
+	    	println(p) 
+	    }
 	  }
 }
